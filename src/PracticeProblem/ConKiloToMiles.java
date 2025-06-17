4d500b62a72ca4b00da65e5002ada2cde115ffa9
praveen-kumar-001 <3978praveen@gmail.com>
2025-06-17 11:43:14 +0530

    [Praveen] fic the issue of folder structure

--- src/PracticeProblem/ConKiloToMiles.java
+++
@@ -1,9 +0,0 @@
-package PracticeProblem;
-import java.util.*;
-public class ConKiloToMiles {
-	 public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        double km = sc.nextDouble();
-	        System.out.println("Miles = "+(km*0.621371));
-	    }
-}
